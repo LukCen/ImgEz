a383e68e18f5d7bc59f059c5dcb83aae943eefd0
lbjcena <lbjcena@gmail.com>
2025-08-27 17:31:20 +0200

    tailwind fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  vite: { plugins: [tailwindcss()] },
   modules: ['@nuxt/ui', '@nuxt/fonts'],
   css: ["~/assets/css/main.css"],
   ui: {
